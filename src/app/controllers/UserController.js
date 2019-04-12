a3952f9421a98e9c21fed218318cfdf675a632c2
Leandro Venancio <leandrohilariovenancio@gmail.com>
2019-04-11 23:40:06 -0300

    Modulo 02 - Aula 11 - upload de arquivos

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,20 @@
+const { User } = require('../models')
+
+class UserController {
+  create (req, res) {
+    return res.render('auth/signup')
+  }
+
+  async store (req, res) {
+    const { filename: avatar } = req.file
+    const deleted_at = null
+
+    await User.create({ ...req.body, avatar, deleted_at })
+
+    console.log(req.body)
+
+    return res.redirect('/')
+  }
+}
+
+module.exports = new UserController()
